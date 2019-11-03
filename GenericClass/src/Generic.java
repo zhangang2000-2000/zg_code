2ee6a8c3d02ba775643eb18ea913a24d12e661b6
zhangang2000-2000 <z2951381>
2019-11-03 21:08:01 +0800

    My understanding about generic class

---
+++ GenericClass/src/Generic.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zhangang on 11/3/19.
+ */
+public class Generic<T> {
+    private  T key;
+
+    public Generic(T key) { //泛型构造方法形参key的类型也为T，T的类型由外部指定
+        this.key = key;
+    }
+
+    public T getKey(){ //泛型方法getKey的返回值类型为T，T的类型由外部指定
+        return key;
+    }
+
+    public static void main(String[] args) {
+        //泛型的类型参数只能是类类型（包括自定义类），不能是简单类型
+        //传入的实参类型需与泛型的类型参数类型相同，即为Integer.
+        Generic<Integer> genericInteger = new Generic<Integer>(123456);
+        System.out.println(genericInteger.getKey());
+    }
+}

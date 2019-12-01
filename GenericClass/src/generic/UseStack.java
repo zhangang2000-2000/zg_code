60e854b343d2f590f88614fa7b7c6764c6315139
zhangang2000-2000 <z2951381>
2019-12-01 21:36:52 +0800

    Try stack class

---
+++ GenericClass/src/generic/UseStack.java
@@ -0,0 +1,13 @@
+package generic;
+
+import java.util.Stack;
+
+/**
+ * Created by zhangang on 12/1/19.
+ */
+public class UseStack {
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<Integer>();
+
+    }
+}
